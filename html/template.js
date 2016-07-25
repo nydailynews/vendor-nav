5c590086a82a5c1d8b2fd3efdc7aea27257967f0
freejoe76 <jmurphy@denverpost.com>
2016-07-25 15:26:31 -0600

    Start on js template

---
+++ html/template.js
@@ -0,0 +1,6 @@
+// This javascript handles writing the site header / footer to a vendor page.
+var vendor_site = {
+    header: '{{header}}',
+    footer: '{{footer}}',
+    init: function() {}  
+};
