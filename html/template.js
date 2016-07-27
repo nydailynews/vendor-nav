e727bcc67c3765c692cb5fa8f4e06062411d3892
freejoe76 <jmurphy@denverpost.com>
2016-07-27 14:52:42 -0600

    Fix font face

--- html/template.js
+++ html/template.js
@@ -47,7 +47,13 @@ var dfmNav = {
         var search_string = 'prod/static/css/denverpost';
         var css_href = 'https://assets.digitalfirstmedia.com/prod/static/css/denverpost.css?ver=1.0';
         this.add_css_if_necessary(search_string, css_href);
+        search_string = 'Source+Sans';
+        css_href = 'https://fonts.googleapis.com/css?family=Source+Serif+Pro%3A400%2C400italic%2C600%2C600italic%2C700%2C700italic%7CSource+Sans+Pro%3A400%2C400italic%2C600%2C600italic%2C700%2C400italic&#038;ver=4.5.3';
+        this.add_css_if_necessary(search_string, css_href);
 
+
+        // Remove existing bartertown CSS
+        jQuery('link[rel=stylesheet][href="http://extras.mnginteractive.com/live/css/site67/bartertown.css"]').remove();
         //jQuery('body').addClass('body-copy');
     },
     initParams: function(params) {
